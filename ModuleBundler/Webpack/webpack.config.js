4e2244c1362ce8612aed70142ea73f27fa41a70b
TedEngineer <tedvu184@gmail.com>
2023-05-06 14:44:06 +1000

    boostrap a simple webpack configs file

---
+++ ModuleBundler/Webpack/webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const path = require("path");
+module.exports = {
+  entry: "./script.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  mode: "production",
+};
