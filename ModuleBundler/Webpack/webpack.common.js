af290d14ac4d6fd6918036957e43d075d0fdcc6a
TedEngineer <tedvu184@gmail.com>
2023-05-21 17:05:20 +1000

    scaffold webpack config files and install webpack merge package

--- ModuleBundler/Webpack/webpack.common.js
+++ ModuleBundler/Webpack/webpack.common.js
@@ -2,10 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./script.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.[hash].js",
-  },
   module: {
     rules: [
       {
--- ModuleBundler/Webpack/webpack.dev.js
+++ ModuleBundler/Webpack/webpack.dev.js
@@ -5,22 +5,7 @@ module.exports = {
   entry: "./script.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.[hash].js",
+    filename: "bundle.js",
   },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        // this has to be in reverse order
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  mode: "development",
   devtool: "source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "template.html",
-    }),
-  ],
 };
